e244f27aa1f51a092376ad43cf8746eafd5a0e8d
Ty <tyler.peterson@utah.edu>
2016-09-28 19:53:25 -0600

    Add filtering logic for streams. Add ajax request for streams.

--- twitch-scripts.js
+++ twitch-scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 //nav
 $(".button-collapse").sideNav();
 
-//twitch channels
+//twitch channels api
 var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404", "MedryBW"];
 var URL = "https://api.twitch.tv/kraken/channels/";
 
@@ -16,16 +16,33 @@ channels.forEach(function(channel) {
 		 },
 		 success: function(data) {
 		   createChannelDiv(channel, data.display_name, data.status, data.logo, data.url);		   		   
-		   console.log(data);
+		   //console.log(data);
 		 },
 		 statusCode:{
-		 	404: function(){
-		 		console.log('404');
+		 	404: function(){		 		
 		 		createChannelDiv(channel, channel, "Page Not Found", "https://www.sonyliv.com/images/6050d5f6.404_icon.png", "No Url");
 		 	}
 		 }
 	});
 }); //end channels.forEach()
+
+//twitch streams api
+var streamURL = "https://api.twitch.tv/kraken/streams/";
+var streams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "MedryBW"];
+
+streams.forEach(function(streams) {
+	$.ajax({
+		 type: 'GET',
+		 url: streamURL + streams,
+		 headers: {
+		   'Client-ID': 'd23sftl55foi7mgw5v9oq1pk8d7bro6'
+		 },
+		 success: function(data) {		  
+		   // console.log(data.stream);
+		   addOnlineStatus(data.stream, streams);
+		 }		
+	});
+}); //end streams.forEach()
 
 // create cards
 function createChannelDiv(channel, name, status, img, url){
@@ -78,6 +95,34 @@ $search.keyup(function(){
 	}
 });
 
+function addOnlineStatus(stream, streamID){
+	if(stream == null){		
+		$('#' + streamID).addClass('isOffline');
+	}else{
+		$('#' + streamID).addClass('isOnline');
+	}
+}
+
+//click on Online filter to view only online channels
+$(".onlineFilter").click(function(){	
+	$('.isOnline').show();
+	$('#comster404').addClass('isOffline');
+	$(".isOffline").toggle();
+});
+
+//click on Offline filter to view only offline channels
+$(".offlineFilter").click(function(){	
+	$('.isOffline').show();
+	$('#comster404').addClass('isOffline');
+	$(".isOnline").toggle();
+});
+
+//click on All to see all channels
+$(".allFilter").click(function(){	
+	$('.isOffline').show();
+	$('#comster404').show();
+	$(".isOnline").show();
+});
 
 }); //end $(document).ready()
 
